344dc3ecf178d95fda754736198b00ed4c2a5a7d
KJ Roelke <kj.roelke@gmail.com>
2025-01-11 09:29:59 -0600

    init admin styles

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
 		entry: {
 			global: `./src/index.ts`,
 			'utilities/bs-utilities': `./src/styles/utilities/bootstrap-utilities.scss`,
+			'admin/login': `./src/styles/admin/login.css`,
 		},
 		plugins: [
 			...defaultConfig.plugins,
