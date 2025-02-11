c904b1bd607a387797cca6a530fb307a3f704b0d
KJ Roelke <kj.roelke@gmail.com>
2025-02-10 20:27:36 -0600

    Release version 1.3.0 with updates to donor list templates, including support for headlines and descriptions, improved styling, and layout adjustments.

--- template-parts/donors/content-donors-multi-list.php
+++ template-parts/donors/content-donors-multi-list.php
@@ -7,21 +7,42 @@
 
 $donor_names   = $args['donor_names'];
 $list_settings = get_field( 'lists' );
-foreach ( $donor_names as $index => $donor_list ) {
-	$is_even           = 0 === $index % 2;
-	$section_classes   = array( 'd-flex', 'flex-column', 'row-gap-5' );
-	$section_classes[] = $is_even ? 'bg-primary text-white py-5' : 'container';
-	echo '<section class="' . esc_attr( implode( ' ', $section_classes ) ) . '">';
-	echo $is_even ? '<div class="container">' : '';
-	echo '<h2 class="text-center mb-5">' . esc_textarea( $list_settings[ $index ]['list_label'] ) . '</h2>';
-	get_template_part(
-		'template-parts/donors/content',
-		'donor-list',
-		array(
-			'donor_names' => array_values( $donor_list )[0],
-			'list_id'     => 'donor-list-' . esc_html( sanitize_title( $list_settings[ $index ]['list_label'] ) ),
-		)
-	);
-	echo $is_even ? '</div>' : '';
-	echo '</section>';
-}
+?>
+<?php foreach ( $donor_names as $index => $donor_list ) : ?>
+	<?php
+	$is_even         = 0 === $index % 2;
+	$section_classes = $is_even ? 'bg-primary text-white py-5' : 'container';
+	$heading_classes = ( $is_even ? 'text-white' : 'text-primary' ) . ' text-center';
+	?>
+<section class="<?php echo esc_attr( $section_classes ); ?>">
+	<?php if ( $is_even ) : ?>
+	<div class="container">
+		<?php endif; ?>
+		<div class="row justify-content-center text-center mb-5">
+			<div class="col-lg-8">
+				<h2 class="<?php echo esc_attr( $heading_classes ); ?>">
+					<?php echo esc_textarea( $list_settings[ $index ]['list_label'] ); ?>
+				</h2>
+				<?php
+				if ( $list_settings[ $index ]['list_description'] ) {
+					echo '<p class="m-0 fs-5">' . esc_textarea( $list_settings[ $index ]['list_description'] ) . '</p>';
+				}
+				?>
+			</div>
+		</div>
+		<?php
+		get_template_part(
+			'template-parts/donors/content',
+			'donor-list',
+			array(
+				'donor_names' => array_values( $donor_list )[0],
+				'list_id'     => 'donor-list-' . esc_html( sanitize_title( $list_settings[ $index ]['list_label'] ) ),
+			)
+		);
+		if ( $is_even ) {
+			echo '</div>';
+		}
+		?>
+</section>
+	<?php
+endforeach;
